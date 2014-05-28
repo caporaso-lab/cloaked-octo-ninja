78c8e5a606997ecc17dcea99e4be2a1f8e844933
Greg Caporaso <gregcaporaso@gmail.com>
2014-05-28 08:07:44 -0700

    updated paths

--- config-env.sh
+++ config-env.sh
@@ -9,12 +9,11 @@ export REF_TREE=/data/gg_13_8_otus/trees/97_otus.tree
 export TINY_TEST_MAP=/home/caporaso/analysis/tiny-test/map
 export TINY_TEST_SEQS=/home/caporaso/analysis/tiny-test/seqs
 
-export MOVING_PICTURES_MAP=/home/caporaso/analysis/moving-pictures/study_550_mapping_file.txt
+export MOVING_PICTURES_MAP=$PROJECT_DIR/sample-metadata/moving-pictures-map.txt
 export MOVING_PICTURES_SEQS=/home/caporaso/analysis/moving-pictures/study_550_split_library_seqs.fna
 
-export SOILS_MAP=/home/caporaso/analysis/88-soils/study_103_mapping_file.txt
+export SOILS_MAP=$PROJECT_DIR/sample-metadata/soils-map.txt
 export SOILS_SEQS=/home/caporaso/analysis/88-soils/study_103_split_library_seqs.fna
 
-export WHOLE_BODY_MAP=/home/caporaso/analysis/whole-body/study_449_mapping_file.txt
+export WHOLE_BODY_MAP=$PROJECT_DIR/sample-metadata/whole-body-map.txt
 export WHOLE_BODY_SEQS=/home/caporaso/analysis/whole-body/study_449_split_library_seqs.fna
-
