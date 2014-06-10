41eadafca8ed10ea1cd83e94c0045c4199728dc4
Greg Caporaso <gregcaporaso@gmail.com>
2014-06-10 08:31:06 -0600

    commented up the config-env.sh script

--- config-env.sh
+++ config-env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# define the ``PROJECT_DIR`` environment variable as
+# the absolute path to your project directory
 export PROJECT_DIR=/home/caporaso/analysis/2014.04.16-ss-otus/
 
+# define the ``REF_DIR`` environment variable as the
+# absolute path to your  Greengenes reference sequences directory
 export REF_DIR=/data/gg_13_8_otus/
 export REF_SEQS_DIR=$REF_DIR/rep_set/
 export REF_TAX_DIR=$REF_DIR/taxonomy/
@@ -23,9 +27,14 @@ export REF_SEQS_82=$REF_SEQS_DIR/82_otus.fasta
 export REF_TAX_82=$REF_TAX_DIR/82_otu_taxonomy.txt
 export REF_TREE_82=$REF_TREE_DIR/82_otus.tree
 
-export TINY_TEST_MAP=/home/caporaso/analysis/tiny-test/map
-export TINY_TEST_SEQS=/home/caporaso/analysis/tiny-test/seqs
+export TINY_TEST_MAP=$PROJECT_DIR/tiny-test/map
+export TINY_TEST_SEQS=$PROJECT_DIR/tiny-test/seqs
 
+# define the ``MOVING_PICTURES_SEQS``, ``SOILS_SEQS``, and ``WHOLE_BODY_SEQS``
+# environment variables as the absolute paths to the post-split-libraries
+# sequence files that you downloaded from the QIIME DB. You do not need to define
+# the ``*_MAP`` environment variables as modified mapping files are included
+# in this repository.
 export MOVING_PICTURES_MAP=$PROJECT_DIR/sample-metadata/moving-pictures-map.txt
 export MOVING_PICTURES_SEQS=/home/caporaso/analysis/moving-pictures/study_550_split_library_seqs.fna
 
